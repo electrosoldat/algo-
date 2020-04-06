ad3e7943604fd19733d9c89305044bad90159760
Tavish Chadha <53138328+electrosoldat@users.noreply.github.com>
2020-04-07 00:03:57 +0530

    Add files via upload

---
+++ DataStructures/Heaps/BuildHeap/BuildHeap.cpp
@@ -0,0 +1,100 @@
+// Code Author : TAVISH CHADHA
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void max_heapify_bottom(vector <int> &heap,int idx)
+{
+	if(idx == 0)
+		return;
+
+	int parent_idx = (idx-1)/2; // parent for 0-based array
+	if(heap[parent_idx] < heap[idx])
+	{
+		// swap the data present at the two positions.
+		// so that, the higher value is at the parent in order to satisfy the max heap property.
+		int temp = heap[parent_idx];
+		heap[parent_idx] = heap[idx];
+		heap[idx] = temp;
+
+		max_heapify_bottom(heap,parent_idx); // now check for the heap property at parent and its parent
+	}
+	else
+	{
+		return;
+	}
+}
+
+void insert(vector <int> &heap,int key)
+{
+	heap.push_back(key);
+	max_heapify_bottom(heap,heap.size()-1);
+}
+
+void display(vector <int> &heap)
+{
+	for(int i=0;i<heap.size();i++)
+	{
+		cout << heap[i] << " ";
+	}
+	cout << endl;
+
+	return;
+}
+
+void max_heapify_top(vector<int>&heap,int idx)
+{
+	int leftidx = 2*idx + 1;
+	int rightidx = 2*idx + 2;
+
+	if(leftidx >= heap.size() && rightidx >= heap.size())
+		return;
+
+	int largestidx = idx;
+	if(leftidx < heap.size() && heap[leftidx]>heap[largestidx])
+		largestidx = leftidx;
+	if(rightidx < heap.size() && heap[rightidx]>heap[largestidx])
+		largestidx = rightidx;
+
+	if(largestidx == idx)
+		return;
+
+	else
+	{
+		swap(heap[idx],heap[largestidx]);
+		max_heapify_top(heap,largestidx);
+	}
+}
+
+void BuildHeap(vector <int> &heap)
+{
+	for(int i=0;i<heap.size();i++)
+	{
+		max_heapify_bottom(heap,i);
+	}
+	// this is the O(n.logn) approach , as it uses upheapify.
+	// due to which more work is done for leaf nodes and lower level nodes.
+}
+
+int main()
+{
+	// Type your code here.
+	vector <int> heap;
+
+	int n;
+	cin >> n;
+
+	for(int i=0;i<n;i++)
+	{
+		int x;
+		cin >> x;
+		heap.push_back(x);
+	}
+
+	BuildHeap(heap);
+	display(heap);
+
+
+	return 0;
+}
